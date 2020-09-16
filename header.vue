4e1d149e0b8cb7df1cb190ad452ee8772ab3cb4d
Henggao Cai <creaturemonster@users.noreply.github.com>
2020-09-15 22:47:35 -0400

    Update header.vue

--- header.vue
+++ header.vue
@@ -1,1 +1,7 @@
 
+<header>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+          <router-link to="/"><Stock Trader/router-link>
+      </div>
